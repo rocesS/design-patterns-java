c30aa230f25206ab739c627f430dc8182c6bdb3b
Karol <karolkrawczyk8650@gmail.com>
2023-03-26 16:24:04 +0200

    BuilderPattern

---
+++ src/main/java/org/example/Builder/Main.java
@@ -0,0 +1,8 @@
+package org.example.Builder;
+
+public class Main {
+    public static void main(String[] args) {
+        NutritionFacts cocaCola = new NutritionFacts.Builder(240, 8)
+                .calories(100).sodium(35).carbohydrate(27).build();
+    }
+}
